eb4642eab2d1581e421c0ba2b86b47652bfcece2
Youra82 <matola@outlook.de>
2025-08-08 11:00:03 +0200

    Update run_envelope.sh

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,2 +1,2 @@
-source LiveTradingBots/code/.venv/bin/activate
-python3 LiveTradingBots/code/strategies/envelope/run.py
+source utbot2/code/.venv/bin/activate
+python3 utbot2/code/strategies/envelope/run.py
