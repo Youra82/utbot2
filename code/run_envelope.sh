e556c4b868889ba50037884013f9503b268ad036
Youra82 <matola@outlook.de>
2025-08-25 22:01:06 +0200

    Update run_envelope.sh

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,2 +1,6 @@
-source utbot2/code/.venv/bin/activate
-python3 utbot2/code/strategies/envelope/run.py
+#!/bin/bash
+# Stellt sicher, dass das Skript im richtigen Verzeichnis ausgeführt wird
+cd /home/ubuntu/utbot2/code/strategies/envelope/
+# Aktiviert die Python-Umgebung und startet den Bot
+source /home/ubuntu/utbot2/code/.venv/bin/activate
+python3 run.py
