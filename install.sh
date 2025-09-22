85bb041e720d5456d3add4ef1164f0ed07c3fabf
Youra82 <matola@outlook.de>
2025-09-22 19:37:35 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "--- Richte gemini-trader ein ---"
+sudo apt-get update -y && sudo apt-get install python3-pip python3-venv -y
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+deactivate
+echo "✔ Einrichtung abgeschlossen. Umgebung '.venv' ist bereit."
