3dba113bde799e2b77ba408656749e673588c98d
Youra82 <matola@outlook.de>
2025-10-22 19:32:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "--- Richte gemini-trader ein ---"
+echo "--- Richte utbot2 (DeepSeek Version) ein ---"
 sudo apt-get update -y && sudo apt-get install python3-pip python3-venv -y
 python3 -m venv .venv
 source .venv/bin/activate
