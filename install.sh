7358593a3cec97621bcd9aba2e0292fa82d71a18
Youra82 <matola@outlook.de>
2025-11-23 08:46:47 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "--- Richte utbot2 (DeepSeek Version) ein ---"
-sudo apt-get update -y && sudo apt-get install python3-pip python3-venv -y
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-deactivate
-echo "✔ Einrichtung abgeschlossen. Umgebung '.venv' ist bereit."
